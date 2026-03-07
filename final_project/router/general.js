7cc1dbb9d506352ebae49de01039e782e4fccc56
Jirotgak <dykjirotgak@gmail.com>
2026-03-07 10:11:41 -0500

    finished the bookReviewProject

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,6 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios');
 
 public_users.post("/register", (req, res) => {
      //Write your code here
@@ -24,67 +25,68 @@ public_users.post("/register", (req, res) => {
   });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  const bookList = JSON.stringify(books, null, 4)
-  return res.status(200).send(bookList);
+const axios = require('axios');
+
+public_users.get('/', async function (req, res) {
+    try {
+        const response = await axios.get("http://localhost:5000/");
+        
+        return res.status(200).send(JSON.stringify(response.data, null, 4));
+    } catch (error) {
+        return res.status(500).json({ message: "Async Error fetching books", error: error.message });
+    }
 });
 
 // Get book details based on ISBN
+const axios = require('axios');
+
 public_users.get('/isbn/:isbn', function (req, res) {
     const isbn = req.params.isbn;
 
-    const book = books[isbn];
-
-    if (book) {
-        return res.status(200).send(JSON.stringify(book, null, 4));
-    } else {
-        return res.status(404).json({ message: "Book not found" });
-    }
+    axios.get(`http://localhost:5000/isbn/${isbn}`)
+        .then((response) => {
+            res.status(200).send(JSON.stringify(response.data, null, 4));
+        })
+        .catch((error) => {
+            const status = error.response ? error.response.status : 500;
+            res.status(status).json({ message: "Book not found or API error" });
+        });
 });
   
 // Get book details based on author
+const axios = require('axios');
+
 public_users.get('/author/:author', function (req, res) {
     const author = req.params.author;
-    
-    const keys = Object.keys(books);
-    
-    let filtered_books = [];
 
-    keys.forEach(key => {
-        if (books[key].author === author) {
-            filtered_books.push(books[key]);
-        }
-    });
-
-    if (filtered_books.length > 0) {
-        return res.status(200).send(JSON.stringify(filtered_books, null, 4));
-    } else {
-        return res.status(404).json({ message: "No books found by this author" });
-    }
+    axios.get(`http://localhost:5000/author/${author}`)
+        .then((response) => {
+            res.status(200).send(JSON.stringify(response.data, null, 4));
+        })
+        .catch((error) => {
+            const status = error.response ? error.response.status : 500;
+            res.status(status).json({ message: "No books found or internal error" });
+        });
 });
 
 // Get all books based on title
+const axios = require('axios');
+
 public_users.get('/title/:title', function (req, res) {
-   const title = req.params.title;
-    
-    const keys = Object.keys(books);
-    
-    let results = [];
-  
-    for (let i = 0; i < keys.length; i++) {
-      let isbn = keys[i];
-      if (books[isbn].title === title) {
-        results.push(books[isbn]);
+  const title = req.params.title;
+
+  // Axios returns a Promise
+  axios.get(`http://localhost:5000/title/${title}`)
+    .then((response) => {
+      return res.status(200).send(JSON.stringify(response.data, null, 4));
+    })
+    .catch((err) => {
+      if (err.response && err.response.status === 404) {
+        return res.status(404).json({ message: "No books found with this title" });
       }
-    }
-  
-    if (results.length > 0) {
-      return res.status(200).send(JSON.stringify(results, null, 4));
-    } else {
-      return res.status(404).json({ message: "No books found with this title" });
-    }
-  });
+      return res.status(500).json({ message: "Error fetching book details", error: err.message });
+    });
+});
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
